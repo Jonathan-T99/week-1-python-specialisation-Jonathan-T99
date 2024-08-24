6144fe7408c10b5d07762b7834bd978de4730784
Jonathan-T99 <jonathantwizere@gmail.com>
2024-08-24 14:46:03 +0200

    week-1-python-specialisation-plp

---
+++ user_input.py
@@ -0,0 +1,22 @@
+# Ask the user for their name
+name=input('Enter your name: ')
+
+# Ask the user for their age
+age=int(input('Enter your age: '))
+
+# Ask the user for their location
+location=input('Enter your location: ')
+
+
+print(f'your name is {name}') #Print out name
+
+print(f'your age is {age}') #Print out age
+
+print(f'your location is {location}') #Print out Location
+
+
+# Print out the personalized message
+print(f'Hello {name}, you are {age} years old and live in {location}')
+
+
+
